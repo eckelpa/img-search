617078420e1d78e293b59f84a849d2c61549cda8
Pascal <aiden90@gmx.de>
2016-04-25 21:59:26 +0000

    cleanup

--- app/controllers/displayResult.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function displayResult(unixdate, normal) {
-    var json = {unixdate: unixdate, normal: normal};
-	return JSON.stringify(json);
-}
-
-module.exports = displayResult;
